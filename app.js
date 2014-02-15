85ad24ce6238b646a62979dec083648f03761712
tnker <yuuya.tanaka@xenophy.com>
2014-02-16 07:41:57 +0900

    update

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ Ext.application({
         'Main'
     ],
 
-    stores: [
-        'Notes'
-    ],
-
     icon: {
         '57': 'resources/icons/Icon.png',
         '72': 'resources/icons/Icon~ipad.png',
